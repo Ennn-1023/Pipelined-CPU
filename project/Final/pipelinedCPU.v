3e03b967f200cc0ab4d4740e336f60b849312007
EnChen <a04182596@gmail.com>
2024-06-04 18:48:18 +0800

    AND checked
    to-do:
    or, add, sub check
    load store check
    jump implementation
    shamt unimplemented

--- project/Final/pipelinedCPU.v
+++ project/Final/pipelinedCPU.v
@@ -4,11 +4,16 @@ module pipelinedCPU(clk, reset);
   wire [31:0] pc, instr_IF, instr_ID;
   wire [5:0] opcode;
   wire [4:0] rs, rt, rd, shamt;
-  wire [5:0] funct;
+  wire [5:0] funct, funct_ID;
   wire [15:0] immed;
   wire [25:0] jumpOffset;
-
   wire [31:0] immed32;
+  wire [31:0] b_Immed;
+  wire [31:0] rd1, rd2;
+  wire [31:0] regWD_WB, regWD_EX;
+  wire [4:0] regWN_WB, regWN_EX, regWN_toMEM;
+  
+  // cut instruction
   assign opcode = instr_ID[31:26];
   assign rs = instr_ID[25:21];
   assign rt = instr_ID[20:16];
@@ -19,6 +24,8 @@ module pipelinedCPU(clk, reset);
   assign jumpOffset = instr_ID[25:0];
   
   // Control signals
+  wire [31:0] ALU_EX;
+  wire Zero_EX;
   wire Branch_ctrl, Branch_ID, Branch_EX;
   wire RegWrite_ctrl, RegWrite_WB, RegWrite_ID, RegWrite_EX;
   wire RegDst_ctrl, RegDst_ID;
@@ -32,7 +39,7 @@ module pipelinedCPU(clk, reset);
   wire [31:0] pc_next, pc_incr, pc_IF, pc_ID, pc_EX, pc_addOffset;
   wire [31:0] pc_jump, pc_branch;
   
-  // ####IF Stage####
+  // ########IF Stage#######
   // PC register
   reg32 PC(.clk(clk), .rst(reset), .en_reg(1'b1), .d_in(pc_next), .d_out(pc));
   // PC increment
@@ -43,27 +50,25 @@ module pipelinedCPU(clk, reset);
 
   // Instruction memory, fetch instruction
   memory instr_mem(.clk(clk), .MemRead(1'b1), .MemWrite(1'b0), .wd(32'd0), .addr(pc), .rd(instr_IF));
-
+  
+  // ########END of IF Stage########
   // Store instruction for next stage
   IF_IDReg IF_ID(.clk(clk), .rst(reset), .enReg(1'b1), .pcIn(pc_incr), .insIn(instr_IF), .pcOut(pc_IF), .insOut(instr_ID));
   
-  /// ###ID Stage###
+  /// ########ID Stage########
   ControlUnit ctrl(.opcode(opcode), .ALUop(ALUop_ctrl), .RegWrite(RegWrite_ctrl), .Branch(Branch_ctrl), 
                    .MemRead(MemRead_ctrl), .MemWrite(MemWrite_ctrl), .MemtoReg(MemtoReg_ctrl), .ALUSrc(ALUSrc_ctrl), 
                    .RegDst(RegDst_ctrl), .Jump(Jump_ctrl));
-  wire [31:0] rd1, rd2;
-  wire [31:0] regWD_WB, regWD_EX;
-  wire [4:0] regWN_WB, regWN_EX, regWN_toMEM;
+  
   reg_file regFile(.clk(clk), .RegWrite(RegWrite_WB), .RN1(rs), .RN2(rt),
                    .WN(regWN_WB), .WD(regWD_WB), .RD1(rd1), .RD2(rd2));
   
   // Extend immediate value to 32-bits
   extend extend32(.immed(immed), .extOut(immed32));
-  
+  // ########End of ID Stage########
   // Store for next stage
   wire [31:0] RD1_ID, RD2_ID, immed_ID;
   wire [4:0] shamt_ID;
-  wire [5:0] funct_ID;
   wire [1:0] ALUop_ID;
   wire [4:0] rd_ID, rt_ID;
   wire ALUSrc_ID;
@@ -77,11 +82,11 @@ module pipelinedCPU(clk, reset);
                  .rd(rd), .rdOut(rd_ID), .RegDst_in(RegDst_ctrl), .RegDst_out(RegDst_ID), .ALUSrc_in(ALUSrc_ctrl),
                  .ALUSrc_out(ALUSrc_ID));
   
-  // ####EX Stage####
+  // ########EX Stage########
   wire [31:0] dataB, ALUOut;
   wire Zero_ALU; // Receive signal from ALU
   mux2 ALU_inputB(.sel(ALUSrc_ID), .a(RD2_ID), .b(immed_ID), .y(dataB));
-  
+  // ------------shamt unimplemented
   // ALU and Zero signal
   TotalALU ALU_MUL(.clk(clk), .rst(reset), .funct(funct_ID), .ALUop(ALUop_ID), .dataA(RD1_ID), .dataB(dataB), .Output(ALUOut), .Zero(Zero_ALU));
   
@@ -89,13 +94,11 @@ module pipelinedCPU(clk, reset);
   mux2 #(5) RN2sel(.sel(RegDst_ID), .a(rt_ID), .b(rd_ID), .y(regWN_toMEM));
   
   // PC+4 add offset << 2
-  wire [31:0] b_Immed;
   // Shift left 2 bits
   shifter offsetSHT(.select(1'b1), .dataIn(immed_ID), .amount(5'd2), .dataOut(b_Immed));
   add32 branchAdder(.a(pc_ID), .b(b_Immed), .result(pc_addOffset)); // PC branch address
   
-  wire [31:0] ALU_EX;
-  wire Zero_EX;
+  // ########End of EX Stage########
   
   EX_MEMReg EX_MEM(.clk(clk), .rst(reset), .enReg(1'b1), .RegWrite_in(RegWrite_ID), .RegWrite_out(RegWrite_EX),
                    .MemtoReg_in(MemtoReg_ID), .MemtoReg_out(MemtoReg_EX), .MemWrite_in(MemWrite_ID), .MemWrite_out(MemWrite_EX),
@@ -103,7 +106,7 @@ module pipelinedCPU(clk, reset);
                    .Zero_in(Zero_ALU), .Zero_out(Zero_EX), .pc_in(pc_addOffset), .pc_out(pc_EX), .ALU_in(ALUOut), .ALU_out(ALU_EX),
                    .WD_in(RD2_ID), .WD_out(regWD_EX), .WN_in(regWN_toMEM), .WN_out(regWN_EX));
   
-  // ####MEM Stage####
+  // ###########MEM Stage############
   wire [31:0] MemRD;
   memory data_mem(.clk(clk), .MemRead(MemRead_EX), .MemWrite(MemWrite_EX), .wd(regWD_EX), .addr(ALU_EX), .rd(MemRD));
   
@@ -112,12 +115,12 @@ module pipelinedCPU(clk, reset);
   and (PCSrc, Branch_EX, Zero_EX);
   
   wire [31:0] RD_MEM, ALU_MEM;
-  
+  // ########End of MEM Stage########
   MEM_WBReg MEM_WB(.clk(clk), .rst(reset), .enReg(1'b1), .RegWrite_in(RegWrite_EX), .RegWrite_out(RegWrite_WB),
                    .MemtoReg_in(MemtoReg_EX), .MemtoReg_out(MemtoReg_MEM), .RD_in(MemRD), .RD_out(RD_MEM),
                    .ALU_in(ALU_EX), .ALU_out(ALU_MEM), .WN_in(regWN_EX), .WN_out(regWN_WB));
   
-  // #####WB Stage#####
+  // ##########WB Stage############
   // Select a result to be written back into register file
   mux2 WriteBackSEL(.sel(MemtoReg_MEM), .a(ALU_MEM), .b(RD_MEM), .y(regWD_WB));
   
